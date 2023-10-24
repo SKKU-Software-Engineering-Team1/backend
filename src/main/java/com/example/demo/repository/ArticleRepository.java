6f58218bf9e431cdf822949c8357013f8459e201
jys8332 <bbogeul123@naver.com>
2023-10-24 13:29:31 +0900

    initial branch

---
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<User, Long> {
+    @Query("select u from User u join fetch u.userTags")
+    List<User> findAllUserTags();
+
+    // 유저의 이메일 정보로 찾는 함수
+    public User findByUserEmail(String UserEmail);
+}
