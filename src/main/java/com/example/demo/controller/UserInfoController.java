cdd7e6e2a67b75cadd0898a6db522f153361ee89
PaikMyeonggyu <qoraudrb123@gmail.com>
2023-11-05 22:10:18 +0900

    2023/11/05

--- src/main/java/com/example/demo/controller/UserInfoController.java
+++ src/main/java/com/example/demo/controller/UserInfoController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.Login.ModifyDto;
+import com.example.demo.dto.Response;
 import com.example.demo.jwt.JwtTokenProvider;
 import com.example.demo.service.Login.LoginService;
+import com.example.demo.service.Login.TokenService;
 import com.example.demo.service.Login.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api")
@@ -15,14 +20,76 @@ public class UserInfoController {
     final JwtTokenProvider jwtTokenProvider;
     final LoginService loginService;
     final UserService userService;
+    final TokenService tokenService;
+    final Response response;
 
     @PostMapping("/UserInfo/userInfo")
-    public ResponseEntity<?> UserInfo(@RequestHeader("Authorization") String accessToken){
-        return userService.getUserInfomation(accessToken);
+    public ResponseEntity<?> UserInfo(@RequestHeader("AccessToken") String accessToken
+                                      , @RequestHeader(value = "refreshToken", required = false) String refreshToken){
+
+        // 모든 코드에 이거 복붙해서 아래 부분 return만 한 줄 본인이 넣으실거 추가하시면 됩니다.
+        // RefreshToken이랑 다 날라가는 거 검증 완료했습니다.
+        if(refreshToken == null){
+            // AccessToken부터가 글러먹었으면 바로 에러를 줍니다.
+            if(!jwtTokenProvider.validateToken(accessToken))
+                return tokenService.requestRefreshToken();
+            else
+                return userService.getUserInformation(accessToken); // 여기만 바꿔주세요
+
+        }
+        else
+            return tokenService.reissueAccessToken(accessToken, refreshToken);
+
     }
 
     @PostMapping("/UserInfo/userShortInfo")
-    public ResponseEntity<?> UserShortInfo(@RequestHeader("Authorization") String accessToken){
-        return userService.getUserTagInfomation(accessToken);
+    public ResponseEntity<?> UserShortInfo(@RequestHeader("AccessToken") String accessToken
+                                           , @RequestHeader(value = "refreshToken", required = false) String refreshToken){
+
+        // 모든 코드에 이거 복붙해서 아래 부분 return만 한 줄 본인이 넣으실거 추가하시면 됩니다.
+        // RefreshToken이랑 다 날라가는 거 검증 완료했습니다.
+        if(refreshToken == null){
+            // AccessToken부터가 글러먹었으면 바로 에러를 줍니다.
+            if(!jwtTokenProvider.validateToken(accessToken))
+                return tokenService.requestRefreshToken();
+            else
+                return userService.getUserTagInformation(accessToken); // 여기만 바꿔주세요
+
+        }
+        else
+            return tokenService.reissueAccessToken(accessToken, refreshToken);
+    }
+
+    @PostMapping("/UserInfo/userDelete")
+    public ResponseEntity<?> UserDelete(@RequestHeader("AccessToken") String accessToken
+            , @RequestHeader(value = "refreshToken", required = false) String refreshToken){
+
+        if(refreshToken == null){
+            // AccessToken부터가 글러먹었으면 바로 에러를 줍니다.
+            if(!jwtTokenProvider.validateToken(accessToken))
+                return tokenService.requestRefreshToken();
+            else
+                return userService.deleteUserInfomation(accessToken); // 여기만 바꿔주세요
+
+        }
+        else
+            return tokenService.reissueAccessToken(accessToken, refreshToken);
+
+    }
+
+    @PostMapping("/UserInfo/ModifyInfo")
+    public ResponseEntity<?> UserModifyInfo(@RequestHeader("AccessToken") String accessToken
+            , @RequestHeader(value = "refreshToken", required = false) String refreshToken, @RequestBody ModifyDto modifyDto){
+
+        if(refreshToken == null){
+            // AccessToken부터가 글러먹었으면 바로 에러를 줍니다.
+            if(!jwtTokenProvider.validateToken(accessToken))
+                return tokenService.requestRefreshToken();
+            else
+                return userService.ModifyUser(accessToken, modifyDto); // 여기만 바꿔주세요
+
+        }
+        else
+            return tokenService.reissueAccessToken(accessToken, refreshToken);
     }
 }
---
+++ src/main/java/com/example/demo/service/Login/TokenService.java
@@ -0,0 +1,45 @@
+package com.example.demo.service.Login;
+
+import com.example.demo.dto.Login.TokenDto;
+import com.example.demo.dto.Login.UserResponseDto;
+import com.example.demo.dto.Response;
+import com.example.demo.entity.Token;
+import com.example.demo.jwt.JwtTokenProvider;
+import com.example.demo.repository.LoginRepository;
+import com.example.demo.repository.UserTagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    final LoginRepository loginRepository;
+    final UserTagRepository userTagRepository;
+    final EntityManager em;
+    final Response response;
+    final JwtTokenProvider jwtTokenProvider;
+
+    public ResponseEntity<?> requestRefreshToken(){
+        return response.fail("AccessToken이 만료되었습니다.", HttpStatus.EXPECTATION_FAILED);
+    }
+
+    public ResponseEntity<?> reissueAccessToken(String AccessToken, String RefreshToken){
+
+        // 1. Refresh Token 검증
+        if (!jwtTokenProvider.validateToken(RefreshToken))
+            return response.fail("Refresh Token 정보가 유효하지 않습니다.", HttpStatus.BAD_REQUEST);
+
+        // 2. RefreshToken은 정상인 경우?
+        Authentication authentication = jwtTokenProvider.getAuthentication(RefreshToken); // Refresh Token의 auth정보 가져옴
+
+        return response.success(jwtTokenProvider.generateToken(authentication));
+
+    }
+
+
+}
